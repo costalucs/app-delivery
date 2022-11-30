7751d5d794743d22283b8b437b2968120123a871
costalucs <lucasdeslz@gmail.com>
2022-11-30 20:54:32 -0300

    feat: get products helper

--- front-end/src/helpers/api/getProducts.js
+++ front-end/src/helpers/api/getProducts.js
@@ -1,5 +1,8 @@
 const { default: axios } = require('axios');
 
-const getProducts = async () => axios.get('/products');
+const getProducts = async () => {
+  const { data } = await axios.get('http://localhost:3001/products');
+  return data;
+};
 
 module.exports = { getProducts };
