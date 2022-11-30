a0891c46cf4ad97103d7c372786ee8bbb4cb6a4b
costalucs <lucasdeslz@gmail.com>
2022-11-30 17:24:23 -0300

    feat: add token to login route api

--- back-end/src/controller/login/index.js
+++ back-end/src/controller/login/index.js
@@ -1,12 +1,16 @@
 const { encode } = require('../../utils/encode');
-const UserService = require('../../services/users')
+const UserService = require('../../services/users');
+const { sign } = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   const passwordMd5 = encode(password)
   const user = await UserService.getOne(email, passwordMd5)
   if (!user) return res.status(404).json({message: "Not found"})
-  return res.status(200).json(user)
+  const token = sign(user.dataValues, jwtSecret)
+  console.log(token);
+  return res.status(200).json({...user.dataValues, token})
 }
 
 module.exports = { login }
