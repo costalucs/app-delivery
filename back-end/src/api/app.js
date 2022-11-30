f4b655be934cd0a40c21382a58cf792adf645e8a
costalucs <lucasdeslz@gmail.com>
2022-11-30 20:54:05 -0300

    chore: add static public images

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -10,6 +10,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static(`public`));
 
 app.get('/products', productsController.getAll)
 app.post('/login', validateLogin, loginController.login)
