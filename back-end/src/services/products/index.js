26dced2154e064d95ffa43d91080c9299cc7fa9f
costalucs <lucasdeslz@gmail.com>
2022-11-30 16:19:06 -0300

    feat: products services

---
+++ back-end/src/services/products/index.js
@@ -0,0 +1,6 @@
+const model = require('../../database/models')
+const getAll = async () => {
+  const allProducts = await model.products.findAll()
+  return allProducts
+}
+module.exports = { getAll }
