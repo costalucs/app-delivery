9afba21fa1aee8516f6b1f65c893769574db56e2
costalucs <lucasdeslz@gmail.com>
2022-11-30 17:42:09 -0300

    feat: add routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Checkout from './pages/Checkout';
 import Login from './pages/Login';
+import MyOrders from './pages/MyOrders';
+import OrderDetails from './pages/OrderDetails';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -9,6 +13,12 @@ function App() {
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Login /> } />
+      <Route path="/customer/products" element={ <Products /> } />
+      <Route path="/customer/checkout" element={ <Checkout /> } />
+      <Route path="/customer/orders/" element={ <MyOrders /> } />
+      <Route path="/seller/orders/" element={ <MyOrders /> } />
+      <Route path="/customer/orders/:id" element={ <OrderDetails /> } />
+      <Route path="/seller/orders/:id" element={ <OrderDetails /> } />
     </Routes>
   );
 }
