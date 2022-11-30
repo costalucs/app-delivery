37f93de2a54507fdb608418970305522ac8288de
costalucs <lucasdeslz@gmail.com>
2022-11-30 16:18:23 -0300

    feat: products controller

---
+++ back-end/src/controller/products/index.js
@@ -0,0 +1,9 @@
+const  productsService  = require('../../services/products')
+
+const getAll = async (_req, res) => {
+  const allProducts = await productsService.getAll();
+  return res.status(200).json(allProducts)
+}
+
+module.exports = { getAll };
+
