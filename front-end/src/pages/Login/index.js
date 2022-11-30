cdcdd01c6024bc445e11805a8adb184e21ac9876
costalucs <lucasdeslz@gmail.com>
2022-11-30 16:16:21 -0300

    [feat]: inicial page

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,46 +1,73 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import { getUserInfo } from '../../helpers/api/login';
 
 function Login() {
-  const location = useLocation();
+  const { pathname } = useLocation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
+  const location = pathname === 'login' ? 'login' : 'register';
+
+  const handleLogin = async () => {
+    console.log('clicou');
+    const userInfo = await getUserInfo(email, password);
+    console.log(userInfo);
+  };
+
   const buttonOptions = location.pathname === '/login'
     ? {
       datatestid: 'common_login__button-login',
       name: 'Login',
       type: 'submit',
-      state: 'true',
+      state: false,
+      handle: handleLogin,
     } : {
       datatestid: 'common_login__button-resgister',
       name: 'Cadastrar',
       type: 'submit',
-      state: 'true',
+      state: false,
+      handle: handleLogin,
     };
+
+  const handleChange = ({ target: { value, name } }) => {
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    if (name === 'name') setUserName(value);
+  };
+
   return (
     <div>
-      {location.pathname === '/register' && <Input
+      {pathname === '/register' && <Input
         datatestid="common_register__input-name"
         label="Nome"
         id="name-input-text"
         type="email"
+        value={ userName }
+        name="name"
         placeHolder="email@email.com"
+        handle={ handleChange }
       />}
       <Input
-        datatestid={
-          location.pathname === '/login'
-            ? 'common_login__input-email' : 'common_register__input-email'
-        }
+        datatestid={ `common_${location}__input-email` }
         label="Login"
-        id="login-input-text"
+        id={ `${location}-input-text` }
         type="email"
+        value={ email }
+        name="email"
         placeHolder="email@email.com"
+        handle={ handleChange }
       />
       <Input
-        datatestid={ location.pathname === '/login'
-          ? 'common_login__input-password' : 'common_register__input-password' }
+        datatestid={ `common_${location}__input-password` }
         label="Password"
-        id="login-input-text"
+        name="password"
+        id={ `${location}-input-text` }
         type="password"
+        value={ password }
+        handle={ handleChange }
         placeHolder="Password"
       />
 
