4df22c557b2f6e9afb3962a1ed63b992f0848c3f
costalucs <lucasdeslz@gmail.com>
2022-11-30 17:12:32 -0300

    fix: app back end

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,21 +3,16 @@ const loginController = require('../controller/login/index')
 const productsController = require('../controller/products');
 const userController = require('../controller/user')
 const { validateLogin } = require('../middleware/validations/validateLogin');
-const cors = require('cors')
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  methods: ['GET', 'POST'],
-}
 
 const app = express();
 
-app.use(cors(corsOptions))
-app.use(express.json());
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.json());
 
 app.get('/products', productsController.getAll)
 app.post('/login', validateLogin, loginController.login)
 app.post('/create', userController.createUser)
-app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
