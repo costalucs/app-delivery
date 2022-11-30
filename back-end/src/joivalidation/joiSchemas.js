c4459605e7a80e0f0072d9c64c702fba3135ba08
costalucs <lucasdeslz@gmail.com>
2022-11-30 16:18:39 -0300

    feat: joi validations

---
+++ back-end/src/joivalidation/joiSchemas.js
@@ -0,0 +1,14 @@
+const joi = require('joi')
+
+const loginSchema = joi.object().keys({
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required()
+})
+
+const userRegisterSchema = joi.object().keys({
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required(),
+  name: joi.string().min(10).required()
+})
+
+module.exports = { loginSchema, userRegisterSchema }
