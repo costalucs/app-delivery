390f70808ec69cc41724a2b366f01df3feec9537
Fael Caporali <rafaelhon@gmail.com>
2022-12-01 19:23:50 -0300

    [feat -  back] dotenv + token update

--- back-end/src/controller/login/index.js
+++ back-end/src/controller/login/index.js
@@ -1,19 +1,21 @@
-const { sign } = require('jsonwebtoken');
+const { sign, verify } = require('jsonwebtoken');
 const { encode } = require('../../utils/encode');
 const UserService = require('../../services/users');
+
+require('dotenv').config();
 
 const jwtSecret = process.env.JWT_SECRET;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   const passwordMd5 = encode(password);
-  const user = await UserService.getOne(email, passwordMd5);
+  const { dataValues: { id, role } } = await UserService.getOne(email, passwordMd5);
 
   if (!user) return res.status(404).json({ message: 'Not found' });
 
-  const token = sign(user.dataValues, jwtSecret);
+  const token = sign({ id, role }, jwtSecret);
 
-  return res.status(200).json({ ...user.dataValues, token });
+  return res.status(200).json({ token });
 };
 
 module.exports = { login };
