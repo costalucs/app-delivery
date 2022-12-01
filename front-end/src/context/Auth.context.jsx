9cf7420dd4f1ceed0376d43214daaa07b15a3a9c
Fael Caporali <rafaelhon@gmail.com>
2022-12-01 20:49:55 -0300

    [feat - front] add auth context

--- front-end/src/context/Auth.context.jsx
+++ front-end/src/context/Auth.context.jsx
@@ -6,30 +6,51 @@ import React, {
   useState,
 } from 'react';
 import PropTypes from 'prop-types';
-import { login, getMe } from '../helpers/api/users';
+import { login as logMe, getMe } from '../helpers/api/users';
 
-const AuthContext = createContext({});
+const AuthContext = createContext({ user: null });
 
 function AuthProvider({ children }) {
-  const [token, setToken] = useState('');
-  const [user, setUser] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
 
-  async function login(email, password) {
-    const token = await login({ email, password });
-    setToken(returnedToken);
-    setUser({ id, name, email, role });
+  async function signIn(email, password) {
+    try {
+      const { token: returnedToken } = await logMe({ email, password });
+      localStorage.setItem('token', returnedToken);
+      setToken(returnedToken);
+      const { name, role, id } = await getMe(token);
+      localStorage.setItem('user', JSON.stringify({ id, name, role }));
+      setUser({ id, name, role });
+    } catch (e) {
+      alert(e);
+    }
   }
 
-  const value = {
-    login,
+  function signOut() {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  const value = useMemo(() => ({
+    signIn,
+    signOut,
     user,
     token,
-  };
+  }), [user, token]);
 
   useEffect(() => {
-    const mounted = true;
+    const localToken = localStorage.getItem('token');
+    if (!localToken) {
+      setUser({});
+      setToken(null);
+    } else {
+      setToken(localToken);
+      setUser(async () => getMe(localToken));
+      localStorage.setItem('user', JSON.stringify(user));
+    }
   }, []);
 
   return (
@@ -38,6 +59,8 @@ function AuthProvider({ children }) {
     </AuthContext.Provider>
   );
 }
+
+export const useUser = () => useContext(AuthContext);
 
 AuthProvider.propTypes = {
   children: PropTypes.element.isRequired,
