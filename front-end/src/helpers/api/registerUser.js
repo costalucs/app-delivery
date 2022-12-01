842099cf1a03b076dc71ae4f022afcea4acb05fc
Fael Caporali <rafaelhon@gmail.com>
2022-12-01 01:13:51 -0300

    [feat-front] signup request validation

--- front-end/src/helpers/api/registerUser.js
+++ front-end/src/helpers/api/registerUser.js
@@ -1,10 +1,13 @@
 const { default: axios } = require('axios');
+const { registerSchema } = require('../validations/credentials');
 
 const registerUser = async (user) => {
   try {
+    const { error } = registerSchema.validate(user);
+    if (error) throw error;
     return await axios.post('/create', user);
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
   }
 };
 
