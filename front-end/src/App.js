f18f2134a93763b8cc6890fca8877abcc80de861
Fael Caporali <rafaelhon@gmail.com>
2022-12-01 20:49:38 -0300

    [refac - front] - Routes separeted

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import MyRoutes from './routes';
+import AuthProvider from './context/Auth.context';
 import './App.css';
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
-import MyOrders from './pages/MyOrders';
-import OrderDetails from './pages/OrderDetails';
-import Products from './pages/Products';
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Navigate to="/login" /> } />
-      <Route path="/login" element={ <Login /> } />
-      <Route path="/register" element={ <Login /> } />
-      <Route path="/customer/products" element={ <Products /> } />
-      <Route path="/customer/checkout" element={ <Checkout /> } />
-      <Route path="/customer/orders/" element={ <MyOrders /> } />
-      <Route path="/seller/orders/" element={ <MyOrders /> } />
-      <Route path="/customer/orders/:id" element={ <OrderDetails /> } />
-      <Route path="/seller/orders/:id" element={ <OrderDetails /> } />
+      <AuthProvider>
+        <MyRoutes />
+      </AuthProvider>
     </Routes>
   );
 }
---
+++ front-end/src/routes/index.js
@@ -0,0 +1,22 @@
+import { Route, Navigate } from 'react-router-dom';
+import Checkout from '../pages/Checkout';
+import Login from '../pages/Login';
+import MyOrders from '../pages/MyOrders';
+import OrderDetails from '../pages/OrderDetails';
+import Products from '../pages/Products';
+
+export default function MyRoutes() {
+  return (
+    <>
+      <Route path="/" element={ <Navigate to="/login" /> } />
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/register" element={ <Login /> } />
+      <Route path="/customer/products" element={ <Products /> } />
+      <Route path="/customer/checkout" element={ <Checkout /> } />
+      <Route path="/customer/orders/" element={ <MyOrders /> } />
+      <Route path="/seller/orders/" element={ <MyOrders /> } />
+      <Route path="/customer/orders/:id" element={ <OrderDetails /> } />
+      <Route path="/seller/orders/:id" element={ <OrderDetails /> } />
+    </>
+  );
+}
