cf8d22520b36687c0c856b40b3db1deb7723f25a
costalucs <lucasdeslz@gmail.com>
2022-12-02 10:17:06 -0300

    [refactor: frontend]: yup credentials

--- front-end/src/helpers/validations/credentials.js
+++ front-end/src/helpers/validations/credentials.js
@@ -1,15 +1,15 @@
-import joi from 'joi';
+import * as yup from 'yup';
 
 const MIN_PASSWORD_LENGTH = 6;
 const MIN_NAME_LENGTH = 12;
 
-export const loginSchema = joi.object({
-  email: joi.string().email({ tlds: { allow: false } }).required(),
-  password: joi.string().min(MIN_PASSWORD_LENGTH).required(),
+export const loginSchema = yup.object({
+  email: yup.string().email({ tlds: { allow: false } }).required(),
+  password: yup.string().min(MIN_PASSWORD_LENGTH).required(),
 });
 
-export const registerSchema = joi.object({
-  email: joi.string().email({ tlds: { allow: false } }).required(),
-  password: joi.string().min(MIN_PASSWORD_LENGTH).required(),
-  name: joi.string().min(MIN_NAME_LENGTH).required(),
+export const registerSchema = yup.object({
+  email: yup.string().email({ tlds: { allow: false } }).required(),
+  password: yup.string().min(MIN_PASSWORD_LENGTH).required(),
+  name: yup.string().min(MIN_NAME_LENGTH).required(),
 });
