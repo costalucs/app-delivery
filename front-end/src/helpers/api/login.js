a395d8878c7cdf95dc2170f4355dd65706e46ee1
costalucs <lucasdeslz@gmail.com>
2022-12-02 11:12:13 -0300

    [refactor: frontend]: remove validation fromhelper

--- front-end/src/helpers/api/login.js
+++ front-end/src/helpers/api/login.js
@@ -1,14 +1,11 @@
 const { default: axios } = require('axios');
-const { loginSchema } = require('../validations/credentials');
 
 const getUserInfo = async (email, password) => {
   try {
-    const { error } = loginSchema.validate({ email, password });
-    if (error) throw error;
     const { data } = await axios.post('http://localhost:3001/login', { email, password });
     return data;
   } catch (e) {
-    alert(e.message);
+    throw new Error(e);
   }
 };
 
