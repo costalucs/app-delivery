b2b45a4a4eb59909f463f248d369e3a7864c96c4
Fael Caporali <rafaelhon@gmail.com>
2022-12-02 22:34:55 -0300

    [factor] [front] removed doubled exception treatment

--- front-end/src/helpers/api/users.js
+++ front-end/src/helpers/api/users.js
@@ -2,34 +2,19 @@ import api from '.';
 import { loginSchema, registerSchema } from '../validations/credentials';
 
 export const login = async (credentials) => {
-  try {
-    const { error } = loginSchema.validate(credentials);
-    if (error) throw error;
-    const { data: { token } } = await api.post('login', credentials);
-    return token;
-  } catch (error) {
-    console.log(error);
-    return { error };
-  }
+  const { error } = loginSchema.validate(credentials);
+  if (error) throw error;
+  const { data: { token } } = await api.post('login', credentials);
+  return token;
 };
 
 export const getMe = async (token) => {
-  try {
-    const { data } = await api.get('/me', { headers: { authorization: token } });
-    return data;
-  } catch (error) {
-    console.log(error);
-    return { error };
-  }
+  const { data } = await api.get('/me', { headers: { authorization: token } });
+  return data;
 };
 
 export const registerUser = async (user) => {
-  try {
-    const { error } = registerSchema.validate(user);
-    if (error) throw error;
-    return await api.post('/create', user);
-  } catch (error) {
-    console.log(error);
-    return { error };
-  }
+  const { error } = registerSchema.validate(user);
+  if (error) throw error;
+  return api.post('/create', user);
 };
