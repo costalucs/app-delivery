13bf9109ed70feff904b91187092b9613de2514e
costalucs <lucasdeslz@gmail.com>
2022-12-07 16:23:16 -0300

    [refactor: frontend]: remove calcCartTotalfrompage

--- front-end/src/pages/Products/index.jsx
+++ front-end/src/pages/Products/index.jsx
@@ -8,23 +8,7 @@ import { getProducts } from '../../helpers/api/products';
 export default function Products() {
   const navigate = useHistory();
   const [items, setItems] = useState();
-  const cart = useCart();
-  const [cartValue, setCartValue] = useState(0);
-
-  function calcCartTotalValue(checkoutCart) {
-    let total = 0;
-
-    if (!checkoutCart[0]) return setCartValue(0);
-
-    checkoutCart.forEach(({ price, quantity }) => {
-      total += (price * quantity);
-    });
-    return setCartValue(total);
-  }
-
-  useEffect(() => {
-    calcCartTotalValue(cart.cart);
-  }, [cart]);
+  const { totalValue } = useCart();
 
   useEffect(() => {
     getProducts().then((data) => setItems(data));
@@ -45,14 +29,14 @@ export default function Products() {
           type="button"
           data-testid="customer_products__button-cart"
           onClick={ checkoutButton }
-          disabled={ cartValue === 0 }
+          disabled={ totalValue === 0 }
         >
           Carrinho:
           {' '}
           <span
             data-testid="customer_products__checkout-bottom-value"
           >
-            { `${cartValue.toFixed(2).replace(/\./, ',')}` }
+            { `${totalValue.toFixed(2).replace(/\./, ',')}` }
           </span>
         </button>
       </div>
