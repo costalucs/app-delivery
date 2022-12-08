facc6cca33e74b2d975994f053dfc157f5571c03
Fael Caporali <rafaelhon@gmail.com>
2022-12-07 22:41:17 -0300

    [feat - front] insert context for seller route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import SellerProvider from './context/Seller.context';
 import Checkout from './pages/Checkout';
 import Login from './pages/Login';
-// import MyOrders from './pages/MyOrders';
+import MySales from './pages/MySales';
 import OrderDetails from './pages/OrderDetails';
 import Products from './pages/Products';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/login" />
-      </Route>
-      <Route exact path="/login" component={ Login } />
-      <Route exact path="/register" component={ Login } />
-      <Route exact path="/customer/products" component={ Products } />
-      <Route exact path="/customer/checkout" component={ Checkout } />
-      {/* <Route exact path="/customer/orders/" component={ MyOrders } /> */}
-      {/* <Route exact path="/seller/orders/" component={ MyOrders } /> */}
-      <Route exact path="/customer/orders/:id" component={ OrderDetails } />
-      <Route exact path="/seller/orders/:id" component={ OrderDetails } />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route exact path="/login" component={ Login } />
+        <Route exact path="/register" component={ Login } />
+        <Route exact path="/customer/products" component={ Products } />
+        <Route exact path="/customer/checkout" component={ Checkout } />
+        {/* <Route exact path="/customer/orders/" component={ MyOrders } /> */}
+        <Route exact path="/customer/orders/:id" component={ OrderDetails } />
+        <Route exact path="/seller/orders/:id" component={ OrderDetails } />
+      </Switch>
+      <SellerProvider>
+        <Switch>
+          <Route exact path="/seller/orders/" component={ MySales } />
+        </Switch>
+      </SellerProvider>
+    </>
   );
 }
 
