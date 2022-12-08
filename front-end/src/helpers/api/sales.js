fb5b585026e5e55626365a73f3758dea0ceb9b99
Fael Caporali <rafaelhon@gmail.com>
2022-12-07 23:10:24 -0300

    [feat - front] init conditional render

--- front-end/src/helpers/api/sales.js
+++ front-end/src/helpers/api/sales.js
@@ -1,9 +1,16 @@
 import api from '.';
 
+export const getMyOrders = async (token) => {
+  const orders = await api.get(
+    '/customers/orders',
+    { headers: { authorization: token } },
+  );
+  console.log(orders);
+  return orders.data;
+};
+
 export const getMySales = async (token) => {
   const sales = await api.get('/seller/orders', { headers: { authorization: token } });
   console.log(sales);
   return sales.data;
 };
-
-export const dummy = () => null;
