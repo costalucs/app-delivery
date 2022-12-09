747f87542cd5e7e9228cf5a393a684fa408c12c2
Fael Caporali <rafaelhon@gmail.com>
2022-12-09 09:50:45 -0300

    [fix - front] formatDate -> fix month + locale add

--- front-end/src/helpers/formatDate.js
+++ front-end/src/helpers/formatDate.js
@@ -2,8 +2,8 @@ export default function formatDate(dateString) {
   const date = new Date(dateString);
   date.setHours(0, 0, 0, 0);
   return [
-    (date.getDate()).toString().padStart(2, '0'),
-    (date.getMonth()).toString().padStart(2, '0'),
+    (date.getDate()).toLocaleString().padStart(2, '0'),
+    (date.getMonth() + 1).toLocaleString().padStart(2, '0'),
     date.getFullYear(),
   ].join('/');
 }
